d917a9d5e722c9aaa7de4268a1bda7d68fe3d479
Brando <jose.brando.c.m@gmail.com>
2021-11-01 13:38:39 -0600

    [CHANGE] Show components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'paisesApp';
+  
+  public option: string = 'buscarPais';
+
+  public onOption(option: string): void {
+    this.option = option;
+  }
+
 }

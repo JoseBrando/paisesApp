2177bd677b588159384e4198663bfaf4914b945b
Brando <jose.brando.c.m@gmail.com>
2021-11-01 14:16:24 -0600

    [CHANGE] Sidebar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 // Components
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -14,7 +15,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }

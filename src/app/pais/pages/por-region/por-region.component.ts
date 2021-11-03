1f38c37ef17eb6f7b32abfc68cd3b73a9d6c1b89
Brando <jose.brando.c.m@gmail.com>
2021-11-03 16:13:40 -0600

    Clases condicionales

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,30 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
-  ]
+  styles: [`
+    button {
+      margin-right: 10px;
+    }
+  `]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
 
-  public hayError = false;
-  public paises: any[] = [];
-  public termino: string = '';
-  public componente: string = 'Por region';
+  public regiones: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public regionActiva: string = '';
+  // public hayError = false;
+  // public paises: any[] = [];
+  // public termino: string = '';
+  // public componente: string = 'Por region';
 
   constructor() { }
 
-  ngOnInit(): void {
+  public activarRegion( region: string ): void {
+    this.regionActiva = region;
   }
 
-  public respuestaService(paises: []): void {
-    this.paises = paises;
+  public getClaseCSS(region: string): string {
+    return (region === this.regionActiva) ? 'btn btn-primary' : 'btn btn-outline-primary';
   }
 
-  public respuestaError(obj: any): void {
-    this.hayError = obj.error;
-    this.termino = obj.termino;
-  }
+  // public respuestaService(paises: []): void {
+  //   this.paises = paises;
+  // }
+
+  // public respuestaError(obj: any): void {
+  //   this.hayError = obj.error;
+  //   this.termino = obj.termino;
+  // }
 
 }

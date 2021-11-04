d0ccbba2e82b87e15fe7a4e3adcf6940cd86f03d
Brando <jose.brando.c.m@gmail.com>
2021-11-03 21:08:47 -0600

    END

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+// Service
+import { PaisService } from '../../services/pais.service';
+
 
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [
-  ]
+  styles: [`
+    li {
+      cursor: pointer;
+    }
+  `]
 })
 export class PorPaisComponent implements OnInit {
 
   public hayError = false;
   public paises: any[] = [];
+  public paisesSugeridos: any[] = [];
   public termino: string = '';
   public componente: string = 'Por pais';
 
-  constructor() { }
+  constructor(private paisService: PaisService) { }
 
   ngOnInit(): void {
   }
@@ -31,6 +38,11 @@ export class PorPaisComponent implements OnInit {
 
   public sugerencias(termino: string): void {
     this.hayError = false;
+    // this.termino = termino;
+
+    this.paisService.buscarPais(termino)
+    .subscribe(paises => this.paisesSugeridos = paises.slice(0, 10),
+               err => this.paisesSugeridos = []);
   }
 
 }
